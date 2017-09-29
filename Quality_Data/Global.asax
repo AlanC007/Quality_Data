28575534d7cfa3e37d771708739d64626eace80b
acannon <acannon@DEVAUTOSQL01>
2017-09-29 16:36:06 -0400

    Add project files.

---
+++ Quality_Data/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Quality_Data.Global" Language="C#" %>
---
+++ Quality_Data/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Quality_Data.Startup))]
+namespace Quality_Data
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
